e0bb828541ec9209f2b03d7700592ac650cf4fbf
galiaorden@outlook.com <galiaorden@outlook.com>
2023-06-02 16:24:13 +0300

    checking

--- src/home.js
+++ src/home.js
@@ -1,36 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
 
 const Home = () => {
- // const [username, setName] = useState("");
-const username=localStorage.userNow;
+  // const [username, setName] = useState("");
+  const username = localStorage.userNow;
   return (
-    <>   
-
-    <p>hello {username}</p>
+    <>
+      <p>hello {username}</p>
     </>
-  )
+  );
 };
 
+// return (
+//     <div >
 
+//         {/* <div>
+//                 <nav id="navigation">
+//                     <a id="logOut" onclick="logOut()" href="home.html" class="hide">LogOut</a>
+//                     <a id="Info" href="info.html" class="hide">Info</a>
+//                     <a id="Todos" href="todos.html" class="hide">Todos </a>
+//                     <a id="Albums" href="albums.html" class="hide">Albums </a>
+//                     <a id="Posts" href="photos.html" class="hide">Posts </a>
+//                 </nav>
+//             </div> */}
 
-
-    // return (
-    //     <div >
-
-    //         {/* <div>
-    //                 <nav id="navigation">
-    //                     <a id="logOut" onclick="logOut()" href="home.html" class="hide">LogOut</a>
-    //                     <a id="Info" href="info.html" class="hide">Info</a>
-    //                     <a id="Todos" href="todos.html" class="hide">Todos </a>
-    //                     <a id="Albums" href="albums.html" class="hide">Albums </a>
-    //                     <a id="Posts" href="photos.html" class="hide">Posts </a>
-    //                 </nav>
-    //             </div> */}
-
-    //     </div>
-    // )
-
-
-
+//     </div>
+// )
 
 export default Home;
