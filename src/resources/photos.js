0b0bb25426e69d0139297521a893e123631c2139
galiaorden@outlook.com <galiaorden@outlook.com>
2023-06-09 15:40:31 +0300

    we finished with the photos. adding 2 buttons for next and previous pictures

--- src/resources/photos.js
+++ src/resources/photos.js
@@ -3,20 +3,21 @@ import { Link, useParams } from "react-router-dom";
 
 const Photos = () => {
   const { idUser, idAlbum } = useParams();
-  console.log(idUser, idAlbum);
   const [filteredData, setFilteredData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPicture, setCurrentPicture] = useState(0);
+  const [currentPictureNum, setCurrentPictureNum] = useState(0);
 
   useEffect(() => {
-    let filteredPhotos;
+    //let filteredPhotos;
     fetch("https://jsonplaceholder.typicode.com/photos")
       .then((response) => response.json())
       .then((data) => {
-        filteredPhotos = data.filter((item) => item.albumId == idAlbum );
-        console.log(idAlbum +"id album");
-        console.log(filteredPhotos)
-
+        let filteredPhotos = data.filter((item) => item.albumId == idAlbum);
         setFilteredData(filteredPhotos);
+        setCurrentPicture(filteredPhotos[currentPictureNum]);
+        console.log(filteredData);
+        console.log(filteredData[0]);
         setLoading(false);
       })
       .catch((error) => {
@@ -26,35 +27,54 @@ const Photos = () => {
       });
   }, [idAlbum]);
 
+  const goNextPicture = () => {
+    setCurrentPictureNum((prevPictureNum) => {
+      let nextPictureNum = prevPictureNum + 1;
+      if (nextPictureNum >= filteredData.length) {
+        nextPictureNum = 0;
+      }
+      setCurrentPicture(filteredData[nextPictureNum]);
+      return nextPictureNum;
+    });
+  };
 
+  const goPreviousPicture = () => {
+    setCurrentPictureNum((prevPictureNum) => {
+      let previousPictureNum = prevPictureNum - 1;
+      if (previousPictureNum < 0) {
+        previousPictureNum = filteredData.length - 1;
+      }
+      setCurrentPicture(filteredData[previousPictureNum]);
+      return previousPictureNum;
+    });
+  };
   return (
     <>
       {/* <Outlet /> */}
-      <Link to={`/users/${idUser}/layout`}>Go back</Link>
+      <Link to={`/users/${idUser}/albums`}>Go back</Link>
       <h1>
         Photos of album {idAlbum} of {localStorage.currentUsername}:
       </h1>
 
       {loading ? (
         <p>Loading...</p>
-      ) : filteredData.length > 0 ? (
-        <ul>
-          {filteredData.map((item) => (
-            <li key={item.id}>
-              <strong>ID:</strong> {item.id}
-              <br />
-              <strong>Title:</strong> {item.title}
-              <br />
-              <strong>Url:</strong> {item.url}
-              <br />
-              <strong>thumbnailUrl:</strong> {item.thumbnailUrl}
-              <br />
-              <br />
-            </li>
-          ))}
-        </ul>
+      ) : currentPicture ? (
+        <div key={currentPicture.id}>
+          <strong>ID:</strong> {currentPicture.id}
+          <br />
+          <strong>Title:</strong> {currentPicture.title}
+          <br />
+          <button onClick={goPreviousPicture}>◀</button>
+          <button onClick={goNextPicture}>▶</button>
+          <br />
+          <img
+            src={currentPicture.thumbnailUrl}
+            style={{ height: "70vh" }}
+            alt="Thumbnail"
+          />
+        </div>
       ) : (
-        <p>No photos found for the specified album.</p>
+        <p>No more photos.</p>
       )}
     </>
   );
